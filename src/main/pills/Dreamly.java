ed64243dddf05ccd6bfdf7cccdb3610586b74d05
Chris-Cole-1 <hartcj2@appstate.edu>
2023-02-20 14:40:10 -0500

    Implemented the Dreamly class

---
+++ src/main/pills/Dreamly.java
@@ -0,0 +1,45 @@
+package main.pills;
+
+public class Dreamly extends GelCap 
+{
+	/*
+	 * Constructor for class Dreamly that calls the super
+	 * constructor of the GelCap class.
+	 *
+	 * @param strength. Pill strength.
+	 * @param size. Pill size. 
+	 * @param strength. Pill color. 
+	 *
+	 */
+	public Dreamly (double strength, double size, String color) 
+	{
+		super(strength, size, color);
+	}
+
+	/*
+	 * This mehtod adds casing to the Dreamly pill.
+	 */
+	protected void addCasing() 
+	{
+		casing = "plasticizer";
+		System.out.println("adding " + casing + " casing");
+	}
+
+	/*
+	 * This method adds a solution to the Dreamly pill.
+	 */
+	protected void addSolution() 
+	{
+		solution = "oil";
+		System.out.println("adding " + solution + " solution");
+	}
+
+	/*
+	 * This method adds an active to the Dreamly pill.
+	 */
+	protected void addActive() 
+	{
+		active = "zolpiem";
+		System.out.println("adding " + active + " active");
+	}
+}
