9665bda4780ca7472a45e6e7b741ac33693f927b
Brock <brockld@appstate.edu>
2023-02-22 14:51:06 -0500

    Finished typing but I still need to test

--- src/tests/pills/AcheAwayTest.java
+++ src/tests/pills/AcheAwayTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AcheAwayTest{
@@ -10,12 +13,23 @@ public class AcheAwayTest{
     public static final String TEST_STRENGTH    = "2.00"
     public static final String TEST_COLOR       = "Red"
     public static final String TEST_SIZE        = "5.00"
+    public static final String MANUFACTURE_FSTRING = "Manufacturing... \nadding Gelatin casing\nadding Saline solution\nadding Acetaminophen active\n... completed manufacturing\n";
 
     private AcheAway ac;
+    private ByteArrayOutputStream baos;
+    private PrintStream oldOut;
     
     @BeforeEach
     public void beforeEach(){
-        ac = new AcheAway(TEST_STRENGTH, TEST_SIZE, TEST_COLOR);
+        this.ac = new AcheAway(TEST_STRENGTH, TEST_SIZE, TEST_COLOR);
+        this.oldOut = System.out;
+        this.baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+    }
+
+    @AfterEach
+    public void AfterEach(){
+        System.setOut(oldOut);
     }
 
     @Test
@@ -25,6 +39,42 @@ public class AcheAwayTest{
 
     @Test
     public void testStength(){
-        
+        assertEquals(TEST_STRENGTH, ac.getStrength());
+    }
+
+    @Test
+    public void testSize(){
+        assertEquals(TEST_SIZE, ac.getSize());
+    }
+
+    @Test
+    public void testColor(){
+        assertEquals(TEST_COLOR, ac.getColor());
+    }
+
+    @Test
+    public void testCasing(){
+        assertEquals(CORRECT_CASING, ac.getCasing());
+    }
+
+    @Test
+    public void testSolution(){
+        assertEquals(CORRECT_SOLUTION, ac.getSolution());
+    }
+
+    @Test
+    public void testActive(){
+        assertEquals(CORRECT_ACTIVE, ac.getActive());
+    }
+
+    @Test
+    public void testManufactureProcess(){
+        ac.manufacture();
+        assertEquals(MANUFACTURE_FSTRING, getOutput());
+    }
+
+    private String getOutput() {
+        System.out.flush();
+        return baos.toString().replaceAll("\r", "");
     }
 }
