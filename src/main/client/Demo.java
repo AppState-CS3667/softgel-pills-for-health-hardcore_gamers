825d7a2b63d117fcb2e05b0a90b223c815f9baf3
aidancook89 <cookar4@appstate.edu>
2023-02-22 14:43:54 -0500

    forgot to save changes

--- src/main/client/Demo.java
+++ src/main/client/Demo.java
@@ -1,17 +1,20 @@
 import pills.AcheAway;
 import pills.Dreamly;
+import pills.GelCap;
 
 
 public class Demo 
 {
     public static void main(String[] args)
     {
-        Dreamly newDreamly = new Dreamly(2.0, 1.5, "Green");
-        AcheAway newAcheAway = new AcheAway(5.0, 0.5, "Red");
+        GelCap newDreamly = new Dreamly(2.0, 1.5, "Green");
+        GelCap newAcheAway = new AcheAway(5.0, 0.5, "Red");
 
+        newDreamly.manufacture();
         System.out.println(newDreamly);
         newDreamly.description();
 
+        newAcheAway.manufacture();
         System.out.println(newAcheAway);
         newAcheAway.description();
     }
