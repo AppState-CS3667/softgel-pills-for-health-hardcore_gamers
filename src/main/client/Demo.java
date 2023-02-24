181b4675005e74eb552c1a892f141cc486475914
aidancook89 <cookar4@appstate.edu>
2023-02-24 14:07:44 -0500

    Wrote Demo class

--- src/main/client/Demo.java
+++ src/main/client/Demo.java
@@ -1,21 +1,18 @@
+package client;
 import pills.AcheAway;
 import pills.Dreamly;
-import pills.GelCap;
 
 
 public class Demo 
 {
     public static void main(String[] args)
     {
-        GelCap newDreamly = new Dreamly(2.0, 1.5, "Green");
-        GelCap newAcheAway = new AcheAway(5.0, 0.5, "Red");
-
-        newDreamly.manufacture();
-        System.out.println(newDreamly);
-        newDreamly.description();
-
-        newAcheAway.manufacture();
+        AcheAway newAcheAway = new AcheAway(5.0, 0.5, "Red");
         System.out.println(newAcheAway);
         newAcheAway.description();
+
+        Dreamly newDreamly = new Dreamly(2.0, 1.5, "Green");
+        System.out.println(newDreamly);
+        newDreamly.description();
     }
 }
