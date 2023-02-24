c90af797045475b1f80a3aa66c35466b60a86781
Savannah Worley <worleysf@appstate.edu>
2023-02-24 14:07:09 -0500

    fixed constructor error in AcheAwayTest

--- src/tests/pills/AcheAwayTest.java
+++ src/tests/pills/AcheAwayTest.java
@@ -11,9 +11,9 @@ public class AcheAwayTest {
     public static final String CORRECT_CASING = "Gelatin";
     public static final String CORRECT_SOLUTION = "Saline";
     public static final String CORRECT_ACTIVE = "Acetaminophen";
-    public static final String TEST_STRENGTH = "2.00";
+    public static final double TEST_STRENGTH = 2.00;
     public static final String TEST_COLOR = "Red";
-    public static final String TEST_SIZE = "5.00";
+    public static final double TEST_SIZE = 5.00;
     public static final String MANUFACTURE_FSTRING = "Manufacturing..."
 						     + "\nadding Gelatin casing\nadding Saline solution"
 						     + "\nadding Acetaminophen active\n... completed"
