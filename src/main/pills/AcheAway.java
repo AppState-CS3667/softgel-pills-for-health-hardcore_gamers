c78d385147107612aa528a9d3203835c0f0a6604
Shawn Lewis <lewissm3@appstate.edu>
2023-03-08 14:50:38 -0500

    Updated AcheAway

--- src/main/pills/AcheAway.java
+++ src/main/pills/AcheAway.java
@@ -1,6 +1,6 @@
 package pills;
 
-public class AcheAway extends GelCap {
+public abstract class AcheAway extends GelCap {
 	/*
 	 * Constructor for the AcheAway class that calls the GelCaps constructor.
 	 * 
@@ -10,32 +10,9 @@ public class AcheAway extends GelCap {
 	 * 
 	 * @param color pill color
 	 */
-    public AcheAway(double strength, double size, String color) {
-        super("AcheAway", strength, size, color); 
+    public AcheAway(double strength, double size, String color,
+	                String casing, String solution, String active) {
+        super(strength, size, color, casing, solution, active); 
 		// calls the GelCaps constructor
-    }
-
-	/*
-	 * Makes the casing for the pill.
-	 */
-    protected void addCasing() {
-        casing = "gelatin";
-        System.out.print("adding " + getCasing() + " casing\n");
-    }
-
-	/*
-	 * Inserts the pill solution into the casing.
-	 */
-    protected void addSolution() {
-        solution = "saline";
-        System.out.print("adding " + getSolution() + " solution\n");
-    }
-
-	/*
-	 * Inserts the active ingredient into the casing.
-	 */
-    protected void addActive() {
-        active = "acetaminophen";
-        System.out.print("adding " + getActive() + " active\n");
     }
 }
