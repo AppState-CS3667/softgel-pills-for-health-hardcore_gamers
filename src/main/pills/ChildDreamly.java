56c8c77894063bda4e2db1ed1472cf02e063e82c
Aidan <cookar4@appstate.edu>
2023-03-18 11:59:52 -0400

    Added public and static identifers to match UML diagram.

--- src/main/pills/ChildDreamly.java
+++ src/main/pills/ChildDreamly.java
@@ -1,9 +1,9 @@
 package pills;
 
 public class ChildDreamly extends Dreamly {
-    final double STRENGTH = 1.25;
-    final double SIZE = 4.5;
-    final String COLOR = "fuchsia";
+    public static final double STRENGTH = 1.25;
+    public static final double SIZE = 4.5;
+    public static final String COLOR = "fuchsia";
     
     /*
      * Constructor for ChildDreamly class that calls the super constructor.
