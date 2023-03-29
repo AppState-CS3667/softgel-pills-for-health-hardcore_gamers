8d030340c7883fab34ae8768b42e14b698e35491
Aidan <cookar4@appstate.edu>
2023-03-29 15:25:37 -0400

    Fixed spelling of pills

--- src/main/pills/GelCapRecipes.java
+++ src/main/pills/GelCapRecipes.java
@@ -10,24 +10,24 @@ public class GelCapRecipes {
     private static HashMap<String, CasingGenerator> getCasings()
     {
         HashMap<String, CasingGenerator> casingsTemp = new HashMap<String, CasingGenerator>();
-        casingsTemp.put("dreamly", new PlasticizerCasing());
-        casingsTemp.put("acheAway", new GelatinCasing());
+        casingsTemp.put("Dreamly", new PlasticizerCasing());
+        casingsTemp.put("AcheAway", new GelatinCasing());
         return casingsTemp;
     }
 
     private static HashMap<String, SolutionGenerator> getSolutions()
     {
         HashMap<String, SolutionGenerator> solutionsTemp = new HashMap<String, SolutionGenerator>();
-        solutionsTemp.put("dreamly", new OilSolution());
-        solutionsTemp.put("acheAway", new SalineSolution());
+        solutionsTemp.put("Dreamly", new OilSolution());
+        solutionsTemp.put("AcheAway", new SalineSolution());
         return solutionsTemp;
     }
 
     private static HashMap<String, ActiveGenerator> getActives()
     {
         HashMap<String, ActiveGenerator> activesTemp = new HashMap<String, ActiveGenerator>();
-        activesTemp.put("dreamly", new ZolpidemActive());
-        activesTemp.put("acheAway", new AcetaminophenActive());
+        activesTemp.put("Dreamly", new ZolpidemActive());
+        activesTemp.put("AcheAway", new AcetaminophenActive());
         return activesTemp;
     }
 }
