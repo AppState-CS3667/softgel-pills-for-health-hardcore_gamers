c4cf166a44b9d3da1e8122b65fe071f7fe315837
Savannah Worley <worleysf@appstate.edu>
2023-03-31 14:39:25 -0400

    finished up store implementation

--- src/main/pills/SoftGelPillStore.java
+++ src/main/pills/SoftGelPillStore.java
@@ -2,6 +2,7 @@ package pills;
 
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SoftGelPillStore {
@@ -14,49 +15,23 @@ public class SoftGelPillStore {
     private int customerAge;
     private boolean isLoggedIn;
 
-    /*
-     * 2 arg constructor
-     * 
-     * @param input user input
-     * @param output class output
-     */
     public SoftGelPillStore(Scanner input, PrintStream output) {
         this.input = input;
         this.output = output;
     }
 
-    /*
-     * 1 arg constructor with output given, creates new scanner with 2 arg constructor
-     * 
-     * @param output class output
-     */
     public SoftGelPillStore(PrintStream output) {
         this(new Scanner(System.in), output);
     }
 
-    /*
-     * 1 arg constructor with input given, sends System.out to 2 arg constructor.
-     * 
-     * @param input user input
-     */
     public SoftGelPillStore(Scanner input) {
         this(input, System.out);
     }
 
-    /*
-     * no arg default constructor sends input and output to 2 arg constructor.
-     */
     public SoftGelPillStore() {
         this(new Scanner(System.in), System.out);
     }
 
-    public void orderGelCap() {
-        //no description for this??
-    }
-
-    /*
-     * returns printed order to customer if logged in.
-     */
     public GelCap[] checkOut() {
         if(isLoggedIn == false || currentOrder == null) {
             System.out.print("You need to log in and order before you can checkout\n");
@@ -66,19 +41,14 @@ public class SoftGelPillStore {
             System.out.print("Thanks for shopping!\nHere is your order\n");
         }
 
-        //idk if this is right, doesn't feel like it
         currentOrder.toString();
 
-        currentOrder.toArray();
+        Object[] orderarr = currentOrder.toArray();
         currentOrder.clear();
 
-        //idk how to return the pills array
-        return GelCap[];
+        return (GelCap[])orderarr;
     }
 
-    /*
-     * prints menu for order selection and produces pills in factory.
-     */
     public void order() {
         if(isLoggedIn == false) {
             System.out.print("You must log in before you can order.\n");
@@ -88,65 +58,107 @@ public class SoftGelPillStore {
             System.out.println("Hello " + customerName + ". What would you like to order?\n");
         }
 
-        System.out.print("Options:\n 1) Dreamly\n 2) AcheAway\n 3) Cancel\n");
-        int choice = input.nextInt();
-        //TODO: check for 1, 2, or 3
+        boolean loop = true;
+        int choice;
+        while(loop)
+        {
+            try
+            {
+                System.out.print("Options:\n 1) Dreamly\n 2) AcheAway\n 3) Cancel\n");
+                choice = input.nextInt();
 
-        //use factory field to produce the type of pill and store in in currentOrder ArrayList
+                if(choice == 1)
+                {
+                    //use the factory field to produce the type of pill selected and store it in the currentOrder ArrayList.
+                    currentOrder.add(factory.produceDreamly());
+                    loop = false;
+                }
+                else if(choice == 2)
+                {
+                    currentOrder.add(factory.produceAcheAway());
+                    loop = false;
+                }
+                else if(choice == 3)
+                {
+                    loop = false;
+                }
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.print("Please enter a 1, 2, or 3\n");
+                input.nextLine();
+            }
+
+        }
     }
 
-    /*
-     * wrapper function asks for name and age to send to logIn.
-     */
     public void logIn() {
         System.out.println("What is your name?");
         String name = input.nextLine();
-        System.out.println("What is your age?");
-        int age = input.nextInt();
-        //TODO: test for valid inputs
+        
+        boolean loop = true;
+        int age = -1;
+        while(loop)
+        {
+            try
+            {
+                System.out.println("What is your age?");
+                age = input.nextInt();
+            }
+            catch(InputMismatchException e)
+            {
+                System.out.print("Please enter a valid age.");
+                input.nextLine();
+            }
+        }
 
         logIn(name, age);
     }
 
-    /*
-     * logs in and directs customer to appropriate factory bassed on age.
-     * creates new order.
-     * 
-     * @param name customer name 
-     * @param age cutsomer age
-     */
     public void logIn(String name, int age) {
         customerName = name;
         customerAge = age;
 
-        if (age < 18) {
-            factory = ChildGelcapFactory;
+        if (customerAge < 18) {
+            factory = new ChildGelCapFactory();
         }
         else {
-            factory = AdultGelCapFactory;
+            factory = new AdultGelCapFactory();
         }
 
         isLoggedIn = true;
         currentOrder = new ArrayList();
     }
 
-    /*
-     * logs out customer if logged in and if order is checked out.
-     * resets customer variables and order.
-     */
     public boolean logOut() {
         if(isLoggedIn == false) {
             System.out.print("You are not logged in.");
             return false;
         }
         else if(currentOrder.size() > 0) {
-            System.out.print("You have an order that you have not checked out. Are you sure you want to log out? (y/N)");
-            String yn = input.nextLine();
-            if(yn == "y") {
-                continue;
-            }
-            else { //TODO: test for bad inputs
-                return false;
+            boolean loop = true;
+            String yn;
+            while(loop)
+            {
+                try
+                {
+                    System.out.print("You have an order that you have not checked out. Are you sure you want to log out? (y/N)");
+                    yn = input.nextLine();
+    
+                    if(yn == "y")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                catch(InputMismatchException e)
+                {
+                    System.out.print("Please enter a valid character.");
+                    input.nextLine();
+                }
             }
         }
         isLoggedIn = false;
@@ -156,34 +168,18 @@ public class SoftGelPillStore {
         return true;
     }
 
-    /*
-     * sets system output.
-     * 
-     * @param output PrintStream output
-     */
     public void setOutput(PrintStream output) {
         this.output = output;
     }
 
-    /*
-     * sets Scanner input.
-     * 
-     * @param input Scanner input
-     */
     public void setInput(Scanner input) {
         this.input = input;
     }
 
-    /*
-     * gets system output.
-     */
     public PrintStream getOutput() {
         return output;
     }
 
-    /*
-     * gets Scanner input.
-     */
     public Scanner getInput() {
         return input;
     }
