ff4653c28b157fda716966fa91fc4d4a6fffd7a3
Shawn Lewis <lewissm3@appstate.edu>
2023-04-03 21:42:16 -0400

    Fixed checkstyle errors and warnings

--- src/tests/pills/CasingTest.java
+++ src/tests/pills/CasingTest.java
@@ -8,10 +8,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class CasingTest {
-    private static final String GEL = "Mixing gelatin, water, opacifier, and glycerin ... \nShaping ... " 
+    private static final String GEL = "Mixing gelatin, water, "
+        + "opacifier, and glycerin ... \nShaping ... " 
                                        + "\nReturning gelatin casing ... \n";
-    private static final String PLAST = "Mixing starch, water, opacifier, and glycerin ... \nShaping ... " 
-                                       + "\nReturning plasticizer casing ... \n";
+    private static final String PLAST = "Mixing starch, water, "
+        + "opacifier, and glycerin ... \nShaping ... " 
+        + "\nReturning plasticizer casing ... \n";
     private static final String GELATIN = "gelatin";
     private static final String PLASTICIZER = "plasticizer";
 
