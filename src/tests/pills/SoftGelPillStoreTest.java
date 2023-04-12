d862e87a20b253828a5ac3a5763db190d83f76c0
Shawn Lewis <lewissm3@appstate.edu>
2023-04-12 13:04:33 -0400

    Started Updates to the StoreTest class

--- src/tests/pills/SoftGelPillStoreTest.java
+++ src/tests/pills/SoftGelPillStoreTest.java
@@ -10,16 +10,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 
 public class SoftGelPillStoreTest {
     // Private Fields
     private static final Scanner INPUT = new Scanner(System.in);
     private static final PrintStream OUTPUT = System.out;
+    private ByteArrayInputStream bais;
+    private ByteArrayOutputStream baos;
+    private PrintStream oldOut;
+    private InputStream oldIn;
     private SoftGelPillStore store;
 
+    @BeforeEach
+    public void beforeEach() {
+        this.store = new SoftGelPillStore();
+        this.oldIn = System.in;
+        this.oldOut = System.out;
+        this.baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.setOut(oldOut);
+        System.setIn(oldIn);
+    }
     // Tests to make sure the constructors works as intended
     @Test
     public void testConstructors() {
@@ -64,24 +89,62 @@ public class SoftGelPillStoreTest {
     // log out without logging in
     @Test
     public void noLogOutWithoutLogin() {
-        this.store = new SoftGelPillStore();
         assertFalse(store.logOut());
     }
     // Test to make sure you can't
     // checkout without logging in
     @Test
     public void noCheckOutWithoutLogin() {
-        this.store = new SoftGelPillStore();
         assertNull(store.checkOut());
+    }
+    // Tests the login with no args method
+    @Test
+    public void successfulLogin() {
+        final String INPUT = "Mr.X\n27\n";
+        bais = new ByteArrayInputStream(INPUT.getBytes());
+        store.logIn();
+        assertTrue(store.logOut());
     }
     // Test to make sure logOut works
     @Test
     public void successfulLogOut() {
-        this.store = new SoftGelPillStore();
         String n = "Mr. X";
-        final int A = 7;
+        final int A = 27;
         store.logIn(n, A);
         assertTrue(store.logOut());
     }
-    
+    // Test make sure the order method is working properly
+    @Test
+    public void test() {
+        String n = "Mr. X";
+        final int A = 27;
+        store.logIn(n, A);
+        final String INPUT = "1";
+        bais = new ByteArrayInputStream(INPUT.getBytes());
+        System.setIn(bais);
+        store.order();
+        store.printCurrentOrder();
+        assertNotEquals("[]", getOutput(),
+            "Should not be empty");
+    }
+    @Test
+    public void test2() {
+        String n = "Mr. X";
+        final int A = 27;
+        store.logIn(n, A);
+        final String INPUT = "1\n1\n1\n";
+        bais = new ByteArrayInputStream(INPUT.getBytes());
+        System.setIn(bais);
+        store.order();
+        store.order();
+        store.order();
+        GelCap[] x = store.checkOut();
+        assertNotNull(x);
+        assertNotEquals("[]", x.toString(),
+            "Should not be empty");
+    }
+    private String getOutput() {
+        System.out.flush();
+        return baos.toString().replaceAll("\r", "");
+    }
 }
